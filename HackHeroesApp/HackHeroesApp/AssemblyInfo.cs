56711eeed823793fb147dd79d570eabd77feeff5
JaworroChanMaker <blazejjaworskixd@gmail.com>
2021-10-18 23:20:55 +0200

    Front-end update #4

--- HackHeroesApp/HackHeroesApp/AssemblyInfo.cs
+++ HackHeroesApp/HackHeroesApp/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("fa-solid-900.otf#Font Awesome 5 Free Solid", Alias = "FAS")]
+[assembly: ExportFont("fa-regular-400.otf", Alias = "FAR")]
+[assembly: ExportFont("fa-brands-400.otf", Alias = "FAB")]
