8805b6acc563e7fad361e9fc458b580fe2d1a34c
Pi0tr71 <92324841+Pi0tr71@users.noreply.github.com>
2021-10-21 21:31:34 +0200

    Backend #4

---
+++ HackHeroesApp/HackHeroesApp/Page9.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HackHeroesApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page9 : ContentPage
+    {
+        public Page9()
+        {
+            InitializeComponent();
+        }
+    }
+}
